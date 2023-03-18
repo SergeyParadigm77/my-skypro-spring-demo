c996f21fffb17caea81b3145e444d73f06fec72c
SergeyParadigm77 <paradigm77@rambler.ru>
2023-03-18 20:32:45 +0300

    Initial commit

---
+++ src/main/java/pro/sky/myskyprospringdemo/MySkyproSpringDemoApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.myskyprospringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySkyproSpringDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MySkyproSpringDemoApplication.class, args);
+	}
+
+}
